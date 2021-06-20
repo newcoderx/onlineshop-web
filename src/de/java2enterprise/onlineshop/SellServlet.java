ce72a7ca9bfb7cf8e5ea377a50622e9ff368566a
newcoderx <homey111@arcor.de>
2021-06-20 11:22:22 +0200

    fotoservice + gitignore

--- src/de/java2enterprise/onlineshop/SellServlet.java
+++ src/de/java2enterprise/onlineshop/SellServlet.java
@@ -5,12 +5,11 @@ import de.java2enterprise.onlineshop.model.Customer;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.time.LocalDateTime;
 
-@WebServlet(name = "SellServlet", value = "/sell")
-@MultipartConfig
+@WebServlet(name = "SellServlet", value = "/sell", asyncSupported = true)
+@MultipartConfig(maxFileSize = 1024*1024*1, maxRequestSize = 1024*1024*30, fileSizeThreshold = 1024*1024, location = "/things")
 public class SellServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -20,26 +19,64 @@ public class SellServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        final String title = request.getParameter("title");
+        final String description = request.getParameter("description");
+        final String price = request.getParameter("price");
+        final Part part = request.getPart("foto");
+
+        InputStream is = part.getInputStream();
+
+        String path = "/my_projects/" + part.getSubmittedFileName();
+        File file = new File(path);
+        OutputStream os = new FileOutputStream(file);
+
         response.setContentType("text/html;charset=UTF-8");
 
-        final Part part = request.getPart("foto");
 
         final PrintWriter out = response.getWriter();
         out.println("<!DOCTYPE html>");
         out.println("<html>");
         out.println("<body>");
-
-
-            out.println("ContentType: " + part.getContentType());
-            out.println("Size: " + part.getSize());
-            out.println("name: " + part.getName());
-            out.println("submitted: " + part.getSubmittedFileName());
-
-
-
-
+        out.println("<br>contype: "+ title);
+        out.println("<br>size: "+ description);
+        out.println("<br>name: "+ price);
+        out.println("<br>submitted: "+ file.getAbsolutePath());
         out.println("</body>");
-        out.println("</html>");
+        out.println("</html>");test
+
+        final AsyncContext ac = request.startAsync();
+        ac.start(new FotoService(is, os));
+        ac.complete();
+
+
+//        String path = part.getSubmittedFileName();
+//        File file = new File(path);
+//        try (OutputStream os = new FileOutputStream(file); InputStream is = part.getInputStream();) {
+//            byte[] b = new byte[1024];
+//            int i = 0;
+//            while ((i = is.read(b)) != -1){
+//                os.write(b, 0, i);
+//            }
+//        } catch (Exception ex){
+//            throw new ServletException(
+//                    ex.getMessage());
+//        }
+
+
+
+        //
+//        final PrintWriter out = response.getWriter();
+//        out.println("<!DOCTYPE html>");
+//        out.println("<html>");
+//        out.println("<body>");
+//        out.println("<br>contype: "+ part.getContentType());
+//        out.println("<br>size: "+ part.getSize());
+//        out.println("<br>name: "+ part.getName());
+//        out.println("<br>submitted: "+ part.getSubmittedFileName());
+//
+//
+//        out.println("</body>");
+//        out.println("</html>");
 
 //        final PrintWriter out = response.getWriter();
 //        out.println("<!DOCTYPE html>");
@@ -53,6 +90,15 @@ public class SellServlet extends HttpServlet {
 //        } else {
 //            out.println("<h1>not logged</h1>");
 //        }
+//
+//        //        application example
+////        ServletContext application = request.getServletContext();
+////        Object objtest = application.getAttribute("customer");
+////        if (objtest != null && objtest instanceof Customer) {
+////            out.println("<h1>testlogged!</h1>");
+////        } else {
+////            out.println("<h1>testnot logged</h1>");
+////        }
 //
 //        out.println("</body>");
 //        out.println("</html>");
